2c9a1332a9063dba5bfe3cb8a99bae67227fb395
arkoGmbH <pm@arko-innovation.ch>
2021-12-02 16:06:53 +0100

    Diverse updates

--- PythonSoftware/02_Tools/04_LinkedInStats/LinkeInStats.py
+++ PythonSoftware/02_Tools/04_LinkedInStats/LinkeInStats.py
@@ -5,75 +5,96 @@
 #  
 #
 # ----------------------
-
+# References:
+# https://realpython.com/python-print/
+# https://www.w3schools.com/python/ref_string_join.asp
+# The join() method takes all items in an iterable and joins them into one string. "separator".join(iterable)
+# string.split(separator, maxsplit) -> Split a string into a list where each word is a list item:
 
 
 import csv
 import json
-from collections import OrderedDict
 
-TBDate='02.Dez.2021'
-TableName='Post_SwitzerlandGlobal' # The name of the table as it will be saved as json file
-TableDescription='Blabla'
+PostDatum='02_Dez_2021'
+PostTitle='Post_SwitzerlandGlobal' # The name of the table as it will be saved as json file
 UniqueHeaderName='name' #Unique header name from which the values will be used as a key for the dictionary
 
 
 # Source CSV file (really comma separated) with commas in the first row
-CSVF='\\Mac\Home\Documents\00_All\3_Arbeit\3_arko_GmbH\9_Projects\01_arko_GmbH\549_Funktionen_Berechnung\03_Marketing\LinkedInTool\BeispielAuswertungEinesPosts.txt' # Text file mit copy paste des LinkedIn Posts
+CSVF='/Users/newmini/Documents/00_All/3_Arbeit/3_arko_GmbH/9_Projects/01_arko_GmbH/GitHub/arkoGmbH/PythonSoftware/02_Tools/04_LinkedInStats/AuswertungEinesPosts.txt' # Text file mit copy paste des LinkedIn Posts
 
 # Directory to export the JSON file
-jsonDir='\\Mac\Home\Documents\00_All\3_Arbeit\3_arko_GmbH\9_Projects\01_arko_GmbH\549_Funktionen_Berechnung\03_Marketing\LinkedInTool\'
+jsonDir='/Users/newmini/Documents/00_All/3_Arbeit/3_arko_GmbH/9_Projects/01_arko_GmbH/GitHub/arkoGmbH/PythonSoftware/02_Tools/04_LinkedInStats/'
 
 # Full directory of the JSON file
-JSNF=jsonDir+TableName+'.json' # Json as a result
-
+JSNF=jsonDir+PostTitle+" "+PostDatum+'.json' # Json as a result
+print('--- Start ----')
+MyData=[]
 # Initialise the dictionary
-data={}
 with open(CSVF, mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    # Convention: Header strarts in the third row (hence line_count=3)
-    line_count = 0
+    csv_reader = csv.reader(csv_file, delimiter=' ') # No delimiter needed in reality
+    i_line = 0
+    i_clear=0
     for row in csv_reader:
-        if line_count == 3:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        #print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-        data[line_count]=row
-        old_key=line_count
-        new_key=data[line_count][str(UniqueHeaderName)]
-        data[new_key] = data[old_key] # Assign a new key to the old key
-        del data[old_key]                # Delete the old key
-        line_count += 1 
-    print(f'Processed {line_count} lines.')
+        # Use only the non empty rows
+        if row:
+            print('Row 0 is: '+ row[0] + ' / Total text: ' + ", ".join(row))
+            MyData.append(", ".join(row))
+            i_clear += 1
+        i_line +=1
+
+# Loop through MyData to get data by company
+Firma= {}
+i=0
+# First row with manual setup
+Number=MyData[i+1].split(",")
+Firma[str(MyData[i])]=Number[0]
+i=2
+
+for x in MyData:
+    if x=="Beruf":
+        print("Du bist bei Beruf")
+        break
+    Number=MyData[i+1].split(",")
+    Firma[str(MyData[i])]=Number[0]
+    i=i+2
+    print(x)
+   
 
 
-# Even more nested dictionary
-evenmore={'Date':TBDate,'Table Name':TableName,'UniqueTableID':UniqueTableID,'Table Description:': TableDescription, 'Stored by ' + str(UniqueHeaderName) :data}
+Beruf= {}
+# Manueles Setup für die erste Zeile
+Number=MyData[15].split(",")
+Beruf[Number[6]]=Number[1]
+i=16
+for x in MyData:
+    if MyData[i]=="Herkunft":
+        print("Du bist bei Herkunft")
+        break
+    Number=MyData[i+1].split(",")
+    Beruf[MyData[i]]=Number[0]
+    i=i+2
+    print(x + ' i: ' + str(i))
 
-# Change the dictionary key from the row number to the desired uniqueHeader one
+Herkunft= {}
+# Manueles Setup für die erste Zeile
+Number=MyData[33].split(",")
+Herkunft[Number[7]]=Number[0]
+i=34
+for x in MyData:
+    if i>=len(MyData)-1:
+        break
+    Number=MyData[i+1].split(",")
+    Herkunft[MyData[i]]=Number[0]
+    i=i+2
+    print(x + ' i: ' + str(i))
 
-#d = {'x':1, 'y':2, 'z':3}
-#d1 = {'x':'a', 'y':'b', 'z':'c'}
 
-#data((d1[key], value) for (key, value) in d.items())
+TotalAnalysis={"PostTitle":PostTitle,"PostDatum":PostDatum,"Firma":Firma,"Beruf":Beruf,"Herkunft":Herkunft}
 
-#data.move_to_end('a')
 #Create json and write data
-with open(JSNF,'w') as jsonFile:
+with open(JSNF,'w', encoding="UTF-8") as jsonFile:
     # make it readable and pretty
-    jsonFile.write(json.dumps(data,indent=4))
+    jsonFile.write(json.dumps(TotalAnalysis,indent=4, ensure_ascii=False))
 
-with open(JSNF,'w') as jsonFile:
-    # make it readable and pretty
-    jsonFile.write(json.dumps(evenmore,indent=4))
-
-# Read the json file (notice that it has a nested dictionary inside on the basis of the row number)
-
-with open(JSNF) as json_file:
-    redata = json.load(json_file)
-    for p in redata['Stored by name']:
-        print('Name: ' + redata['Stored by name'][p]['name'])
-        print('Bday: ' + redata['Stored by name'][p]['birthday month'])
-        print('')
-# p is the unique value that helps to retrieve the information we want. Example to find Erica Meyers birthday we do the following
-value=redata['Stored by name']['Erica Meyers']['birthday month']
+print('---- Ended -----')
