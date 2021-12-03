471304b7108792b7f930bc057f0e04a89a67699d
arkoGmbH <pm@arko-innovation.ch>
2021-12-03 10:42:31 +0100

    Finished the first round of the tool!

--- PythonSoftware/02_Tools/04_LinkedInStats/LinkeInStats.py
+++ PythonSoftware/02_Tools/04_LinkedInStats/LinkeInStats.py
@@ -1,6 +1,6 @@
 #!/Users/newmini/Documents/00_All/3_Arbeit/3_arko_GmbH/9_Projects/01_arko_GmbH/549_Funktionen_Berechnung/07_TrägheitsTool/PythonProject/env/bin/python
 # ----------------
-#  02.12.2021 Petros Mitropoulos, arko GmbH
+#  02.12.2021 Petros Mitropoulos, arko GmbH, all rights reserved
 #  LinkedIn Post daten einlesen und Statistik daraus erstellen
 #  
 #
@@ -11,11 +11,11 @@
 # The join() method takes all items in an iterable and joins them into one string. "separator".join(iterable)
 # string.split(separator, maxsplit) -> Split a string into a list where each word is a list item:
 
-
 import csv
 import json
 
 PostTitle='JSON Format29_11_2021' # The name of the table as it will be saved as json file
+Stichtag='03.12.2021'
 
 Dir='/Users/newmini/Documents/00_All/3_Arbeit/3_arko_GmbH/6_Marketing/00_LinkedIn_Blogs/Auswertung/'
 # Source CSV file (really comma separated) with commas in the first row
@@ -39,27 +39,22 @@ with open(CSVF, mode='r') as csv_file:
             i_clear += 1
         i_line +=1
 
-
 # Loop through MyData to get data by company
 Firma= {}
 i=0
 # First row with manual setup
-Number=MyData[0].split(",")
-Firma[str(MyData[i])]=Number[0]
+Number=MyData[1].split(",")
+Firma[MyData[0]]=Number[0]
 i=2
-j=0
 for i in range(2,len(MyData),2):
-    if MyData[j]=="Beruf":
+    if MyData[i]=="Beruf":
         print("Du bist bei Beruf")
-        StartBeruf=j
+        StartBeruf=i
         break
-    elif MyData[j+1]=="Beruf":
-        print("Du bist bei Beruf")
-        StartBeruf=j+1
-        break
+
     Number=MyData[i+1].split(",")
     Firma[str(MyData[i])]=Number[0]
-    j=j+1
+  
     print(i)
 
 Beruf= {}
@@ -67,36 +62,27 @@ Beruf= {}
 Number=MyData[StartBeruf+1].split(",")
 Beruf[Number[6]]=Number[1]
 j=StartBeruf+2
-for i in range(0,len(MyData),2):
-    if i>StartBeruf+1:
-        if MyData[j]=="Herkunft":
-            print("Du bist bei Herkunft")
-            StartHerkunft=j
-            break
-        elif MyData[j+1]=="Herkunft":
-            print("Du bist bei Herkunft")
-            StartHerkunft=j+1
-            break
-        Number=MyData[i+1].split(",")
-        Beruf[MyData[i]]=Number[0]
-        j=j+1
-        print(i)
+for i in range(StartBeruf+2,len(MyData),2):
+    if MyData[i]=="Herkunft":
+        print("Du bist bei Herkunft")
+        StartHerkunft=i
+        break
+    Number=MyData[i+1].split(",")
+    Beruf[MyData[i]]=Number[0]
+    print(i)
   
-        
-
 Herkunft= {}
 # Manueles Setup für die erste Zeile
 Number=MyData[StartHerkunft+1].split(",")
 Herkunft[Number[7]]=Number[0]
 
-for i in range(0,len(MyData),2):
-    if i>=StartHerkunft+1:
-        Number=MyData[i+1].split(",")
-        Herkunft[MyData[i]]=Number[0]
-        print(i)
+for i in range(StartHerkunft+2,len(MyData)-1,2):
+    Number=MyData[i+1].split(",")
+    Herkunft[MyData[i]]=Number[0]
+    print(i)
 
 
-TotalAnalysis={"PostTitle":PostTitle,"Firma":Firma,"Beruf":Beruf,"Herkunft":Herkunft}
+TotalAnalysis={"PostTitle":PostTitle,"Stichtag":Stichtag,"Firma":Firma,"Beruf":Beruf,"Herkunft":Herkunft}
 
 #Create json and write data
 with open(JSNF,'w', encoding="UTF-8") as jsonFile:
