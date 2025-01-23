5fb7044c1b8c2c28cc6d4d2d9ed303a142edb31b
Petros <72615136+arkoGmbH@users.noreply.github.com>
2025-01-23 22:58:18 +0100

    Example JSON and POST

--- PythonSoftware/02_Tools/06_RemoteInvoicing/RemoteInvoicing2/mainPOST.py
+++ PythonSoftware/02_Tools/06_RemoteInvoicing/RemoteInvoicing2/mainPOST.py
@@ -1,24 +1,5 @@
 import requests
 
+response = requests.post("https://httpbin.org/post", data={"key": "value"}, headers={"Content-Type": "application/json"})
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Start')  # Press Ctrl+F8 to toggle the breakpoint.
-    url = 'https://pmarko.pythonanywhere.com/create_user'
-    payload={'first_name':'Petsdfgaafadaa','last_name':'Psgfaf','save':'save'}
-    r=requests.get(url, payload)
-    if r.status_code == 200:
-        print("Success!")
-    elif r.status_code == 404:
-        print("Not Found.")
-    print(r.request.url)
-    print(f'End')
-
-# Press the green button in the gutter to run the script.
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(response.json())
